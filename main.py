020166c18a4fba20d6ea0a3326debcd8c4219e5f
mathew8e <123kumatan@gmail.com>
2021-10-17 11:25:33 +0200

    It actually completly works

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from os import listdir
+from os import listdir, remove
 import numpy as np
 
 
@@ -20,6 +20,7 @@ def loadImages(path):
 
 def convImgToNpArrays(array):
     #function for creating a np array
+
     print("Started np converting")
     numpyimg = []
     for i in range(len(array)):
@@ -27,25 +28,58 @@ def convImgToNpArrays(array):
     print("Np convert end")
     return numpyimg
 
-# checking each photo with eacho
 def cloneFind(npimgs):
-    #make this an np array so you can have a array in array
-    cloneImgArray = []
+    # checking each photo with eachother
+
+    dictOfIndexes = {}
+    pairArray = {}
+
+    #two indexes will go through all files
     for imgIndex1 in range(len(npimgs)):
         print("checking",imgIndex1," from ", len(npimgs))
         for imgIndex2 in range(len(npimgs)):
-            if np.array_equal(npimgs[imgIndex1], npimgs[imgIndex2]) and imgIndex1 != imgIndex2:
-                print("Found ",imgs[imgIndex1].filename.replace(path, '')," with ",imgs[imgIndex2].filename.replace(path,''))
-                cloneImgArray.append(imgs[imgIndex1].filename.replace(path, ''))
-                cloneImgArray.append(imgs[imgIndex2].filename.replace(path, ''))
 
-    #return cloneImgArray
+            #                 if imgs are the same                      if the img is not itself            if img clone isnt 2 1  1 2
+            if np.array_equal(npimgs[imgIndex1], npimgs[imgIndex2]) and imgIndex1 != imgIndex2 and not(imgIndex1 in dictOfIndexes.values()) and not(imgIndex2 in dictOfIndexes.values()) and not(imgIndex2 in dictOfIndexes.keys()) and not(imgIndex1 in dictOfIndexes.values()):
+
+                img1 = imgs[imgIndex1].filename #.replace(path, '')
+                img2 = imgs[imgIndex2].filename #.replace(path, '') #this replaces the path
+
+                # just some debugging
+                print("Found ",img1," with ",img2)
+
+                #cloneImgArray.append(imgs[imgIndex1].filename.replace(path, ''))
+                #cloneImgArray.append(imgs[imgIndex2].filename.replace(path, ''))
+                
+                #create a array pair
+                #pairArray = [imgs[imgIndex1].filename.replace(path,''),imgs[imgIndex2].filename.replace(path,'')]
+                
+                if img1 in pairArray.keys() :
+                    print(pairArray[img1])
+                    pairArray[img1].append(img2)
+                elif not any([True for k,v in pairArray.items() if img1 in v]):
+                    pairArray[img1] = [img2]
+
+
+                dictOfIndexes[imgIndex1] = imgIndex2
+
+
+    return pairArray
 
     #this return statement is funny lol XD return unique values
-    return list(set(cloneImgArray))
+    #return list(set(cloneImgArrayOfArrays))
+
+def deleteCloneImages(dictOfCloneImages):
+    #this function will delete all unoriginal photos
+
+
+    for k,v in dictOfCloneImages.items():
+        for delImg in v:
+            remove(delImg)
+            print(f"removing {delImg} \n")
 
 #path specified will change later
-path = "C://Users//mathe//Downloads//Images for the website October 9-20211011T180149Z-001//Images for the website October 9//Khlil Chaaban/"
+path = ".//images/"
 
 # images in an regular array
 imgs = loadImages(path)
@@ -55,6 +89,8 @@ npimgs = convImgToNpArrays(imgs)
 
 cloneListArray = cloneFind(npimgs)
 
+
 print(f"\n        Found these images as copy \n  {cloneListArray}")
 
             
+deleteCloneImages(cloneListArray)
